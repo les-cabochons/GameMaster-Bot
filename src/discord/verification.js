dfb03e644e592cfe5c476b76bbfb54c54e762f31
Plekse <alextrepa@gmail.com>
2024-01-23 01:36:27 -0500

    feat(#6): Get headers correctly

--- src/discord/verification.js
+++ src/discord/verification.js
@@ -3,8 +3,8 @@ import { verifyKey } from "discord-interactions";
 const { PUBLIC_KEY } = process.env;
 
 export function verifyDiscordRequest(event) {
-  const signature = event.params.headers.get("x-signature-ed25519");
-  const timestamp = event.params.headers.get("x-signature-timestamp");
+  const signature = event.params.header["x-signature-ed25519"];
+  const timestamp = event.params.header["x-signature-timestamp"];
   const body = event.rawBody;
   const isValidRequest =
     signature &&
